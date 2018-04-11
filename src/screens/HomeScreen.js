23eae3d082f3cac13c1860aff302dd767033c401
Sushil L <sushil_samson@hotmail.com>
2018-04-11 21:19:43 +1000

    added homeImage and homepage design, utils metrics for creating a circle and hitSlop for buttons touchable

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+//import Touchable from '@appandflow/touchable';
+//import { makeCircle, makeHitSlop } from '../utils/metrics';
+
+import homeImage from '../../assets/images/homeImage.jpg';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    backgroundColor: '#ccc',
+    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center'
+  },
+  homeTextWrapper: {
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  homeText: {
+    textAlign: 'center',
+    fontSize: 38
+  },
+  mottoText: {
+    flex: 0.2,
+    textAlign: 'center',
+    fontSize: 15
+  },
+  btnWrapper: {
+    flex: 0.2,
     alignItems: 'center',
     justifyContent: 'center'
   }
@@ -12,8 +43,19 @@ const styles = StyleSheet.create({
 
 const HomeScreen = () => (
   <View style={styles.container}>
-    <Text>HomeScreen</Text>
+    <Image source={homeImage} style={styles.image} />
+    <View style={styles.homeTextWrapper}>
+      <Text style={styles.homeText}>Sustainable Farm</Text>
+    </View>
+    <Text style={styles.mottoText}>For today and future. For us and them.</Text>
   </View>
 );
 
 export default HomeScreen;
+
+// <Touchable
+//       hitSlop={makeHitSlop(20)}
+//       feedback="opacity"
+//       style={styles.btnWrapper}
+//     >
+//     </Touchable>
